7982ce3e74d113b4761478422f689cbe13d75864
progspectar <progspectar@gmail.com>
2022-06-16 20:03:30 +0700

    6.8. Дефрагментация массива [#225 #312606] замер количества итераций counter 124 (Korobeinikov Stas) counter 72 with redundant var (mine)

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -1,10 +1,15 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class Defragment {
     public static String[] compress(String[] array) {
+        int counter = 0;
         for (int index = 0; index < array.length; index++) {
+            counter++;
             if (array[index] == null) {
                 for (int i = index + 1; i < array.length; i++) {
+                    counter++;
                     if (array[i] != null) {
                         array[index] = array[i];
                         array[i] = null;
@@ -13,16 +18,37 @@ public class Defragment {
                 }
             }
         }
+        System.out.println("counter " + counter + " without redundant var (Korobeinikov Stas) " + Arrays.toString(array) + " compress() ");
+        return array;
+    }
+
+    public static String[] compress1(String[] array) {
+        int counter = 0;
+        int pos = 0;
+        for (int index = 0; index < array.length; index++) {
+            counter++;
+            if (array[index] == null) {
+                for (int i = pos + 1; i < array.length; i++) {
+                    counter++;
+                    if (array[i] != null) {
+                        array[index] = array[i];
+                        array[i] = null;
+                        pos = i;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("counter " + counter + " with redundant var (mine) " + Arrays.toString(array) + " compress1() ");
         return array;
     }
 
     public static void main(String[] args) {
-        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed",
+                null, null, null, "right", null, null, "now", null, "straight", "away", null, null, null};
         String[] compressed = compress(input);
-        System.out.println();
-        for (int index = 0; index < compressed.length; index++) {
-            System.out.print(compressed[index] + " ");
-        }
+        String[] input1 = {"I", null, "wanna", null, "be", null, "compressed",
+                null, null, null, "right", null, null, "now", null, "straight", "away", null, null, null};
+        compress1(input1);
     }
-
 }

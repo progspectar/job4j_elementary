2ffb61ef9d4fc5333abdc2929d9672aa7f1068ff
progspectar <progspectar@gmail.com>
2022-06-22 19:11:36 +0700

    Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,7 +1,18 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left > right ? left : right;
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    public int max(int first, int second, int third) {
+        return max(
+                first,
+                max(second, third));
+    }
+
+    public int max(int first, int second, int third, int fourth) {
+        return max(fourth,
+                max(first, second, third));
     }
 }
--- src/main/java/ru/job4j/condition/MaxTest.java
+++ src/main/java/ru/job4j/condition/MaxTest.java
@@ -7,27 +7,28 @@ public class MaxTest {
 
     @Test
     public void whenMax10To2Then10() {
-        int left = 10;
-        int right = 2;
-        int result = Max.max(left, right);
+        int result = new Max().max(10, 2);
         int expected = 10;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax1To20Then20() {
-        int left = 1;
-        int right = 20;
-        int result = Max.max(left, right);
-        int expected = 20;
+    public void whenMax1To20To30Then30() {
+        int result = new Max().max(1, 20, 30);
+        int expected = 30;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax1To1Then1() {
-        int left = 1;
-        int right = 1;
-        int result = Max.max(left, right);
+    public void whenMax15T1To4To5Then5() {
+        int result = new Max().max(15, 1, 4, 5);
+        int expected = 15;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To1To1To1Then1() {
+        int result = new Max().max(1, 1, 1, 1);
         int expected = 1;
         Assert.assertEquals(result, expected);
     }

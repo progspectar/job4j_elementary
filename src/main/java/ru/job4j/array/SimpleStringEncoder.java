3457fe2e7c20cca43f6a327ffec41bc7f994a215
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-05 14:54:33 +0700

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049] сжатие строки

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+        /*
+        цикл fori
+         */
+        return result;
+    }
+}

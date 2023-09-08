9c904b1e062a18205eb23d59707406a8ee9baaa6
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-08 22:01:44 +0700

    Factory Method

---
+++ src/main/java/oop/fabric/Rectangle.java
@@ -0,0 +1,21 @@
+package oop.fabric;
+
+public class Rectangle {
+    int a;
+    int b;
+    public Rectangle(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public String draw() {
+        String ln = System.lineSeparator();
+        return "******" + ln
+                + "*    *" + ln
+                + "*    *" + ln
+                + "******";
+    }
+    public double square() {
+        return a * b;
+    }
+}

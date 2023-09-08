2c068aaac19e91b10a3fc8cba9ac17e22d97240d
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-09 00:47:58 +0700

    тест

--- src/main/java/ru/job4j/queue/AppleStore.java
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -13,24 +13,10 @@ public class AppleStore {
     }
 
     public String getLastHappyCustomer() {
-        if (queue.size() < count) {
-            return "";
-        }
-        String res = "";
-        for (int i = 0; i < count; i++) {
-            res = queue.poll().name();
-        }
-        return res;
+        return "";
     }
 
     public String getFirstUpsetCustomer() {
-        if (queue.size() < count + 1) {
-            return "";
-        }
-        String res = "";
-        for (int i = 0; i < count + 1; i++) {
-            res = queue.poll().name();
-        }
-        return res;
+        return "";
     }
 }

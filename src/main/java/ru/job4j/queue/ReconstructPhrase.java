492bd1295c41c9656423becc2dfa93ea3d5e0f50
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-09 00:55:20 +0700

    test

--- src/main/java/ru/job4j/queue/ReconstructPhrase.java
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -1,7 +1,6 @@
 package ru.job4j.queue;
 
 import java.util.Deque;
-import java.util.Iterator;
 
 public class ReconstructPhrase {
 
@@ -15,24 +14,11 @@ public class ReconstructPhrase {
     }
 
     private String getEvenElements() {
-        StringBuilder res = new StringBuilder();
-        int length = evenElements.size();
-        for (int i = 0; i < length; i++) {
-            char el = evenElements.pollFirst();
-            if ((i % 2) == 0) {
-                res.append(el);
-            }
-        }
-        return res.toString();
+        return "";
     }
 
     private String getDescendingElements() {
-        Iterator<Character> iterator = descendingElements.descendingIterator();
-        StringBuilder res = new StringBuilder();
-        while (iterator.hasNext()) {
-            res.append(iterator.next());
-        }
-        return res.toString();
+        return "";
     }
 
     public String getReconstructPhrase() {

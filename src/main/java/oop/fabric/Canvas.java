ecab4ec40bf93190e634616ffc9d86b6d2439379
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-08 22:31:18 +0700

    Factory Method Canvas

--- src/main/java/oop/fabric/Canvas.java
+++ src/main/java/oop/fabric/Canvas.java
@@ -3,26 +3,27 @@ package oop.fabric;
 import java.util.Scanner;
 
 public class Canvas {
+    private ShapeOperator shapeOperator;
+
+    public Canvas(ShapeOperator shapeOperator) {
+        this.shapeOperator = shapeOperator;
+    }
+
+    public void demonstrate() {
+        this.shapeOperator.showInfo();
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        ShapeOperator operator;
         System.out.print("Какую фигуру вы хотите построить: ");
         String ans = input.nextLine();
         if ("прямоугольник".equals(ans)) {
-            System.out.print("Введите ширину прямоугольника: ");
-            int a = Integer.parseInt(input.nextLine());
-            System.out.print("Введите длину прямоугольника: ");
-            int b = Integer.parseInt(input.nextLine());
-            Rectangle rectangle = new Rectangle(a, b);
-            System.out.println(rectangle.draw());
-            System.out.println("Площадь прямоугольника равна: " + rectangle.square());
+            operator = new RectangleOperator();
         } else {
-            System.out.print("Введите длину основания треугольника: ");
-            int a = Integer.parseInt(input.nextLine());
-            System.out.print("Введите высоту треугольника: ");
-            int b = Integer.parseInt(input.nextLine());
-            Triangle triangle = new Triangle(a, b);
-            System.out.println(triangle.draw());
-            System.out.println("Площадь прямоугольника равна: " + triangle.square());
+            operator = new TriangleOperator();
         }
+        Canvas canvas = new Canvas(operator);
+        canvas.demonstrate();
     }
 }

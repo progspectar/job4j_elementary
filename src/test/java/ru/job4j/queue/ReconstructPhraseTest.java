450e32b7627a7dcebd8e147169f547af16fed7fe
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-09 01:00:06 +0700

    test

--- src/test/java/ru/job4j/queue/ReconstructPhraseTest.java
+++ src/test/java/ru/job4j/queue/ReconstructPhraseTest.java
@@ -1,27 +1,33 @@
 package ru.job4j.queue;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.LinkedList;
 
-public class ReconstructPhrase {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    private final Deque<Character> descendingElements;
+class ReconstructPhraseTest {
 
-    private final Deque<Character> evenElements;
-
-    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
-        this.descendingElements = descendingElements;
-        this.evenElements = evenElements;
-    }
-
-    private String getEvenElements() {
-        return "";
-    }
-
-    private String getDescendingElements() {
-        return "";
-    }
-
-    public String getReconstructPhrase() {
-        return getEvenElements() + getDescendingElements();
+    @Test
+    void whenSloganOfLanguageJava() {
+        Deque<Character> evenElements = new LinkedList<>(
+                Arrays.asList(
+                        'S', '1', 'l', '3', 'o', 'h', 'g', 'h', 'a', 'm', 'n', 't',
+                        ' ', 't', 'o', ' ', 'f', 'q', ' ', '6', 'l', '.', 'a', ',',
+                        'n', 'w', 'g', 'd', 'u', 'a', 'a', 'u', 'g', 'd', 'e', 'q',
+                        ' ', 'y', 'J', 'd', 'a', 'k', 'v', 'p', 'a', 'e', ':', 'b'
+                )
+        );
+        Deque<Character> descendingElements = new LinkedList<>(
+                Arrays.asList(
+                        'e', 'r', 'e', 'h', 'w', 'y', 'r', 'e', 'v', 'e', ' ', 'n', 'u',
+                        'r', ' ', ',', 'e', 'c', 'n', 'o', ' ', 'e', 't', 'i', 'r', 'W', ' '
+                )
+        );
+        String expected = "Slogan of language Java: Write once, run everywhere";
+        String result = new ReconstructPhrase(descendingElements, evenElements).getReconstructPhrase();
+        assertThat(result).isEqualTo(expected);
     }
 }

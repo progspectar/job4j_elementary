c4120b735d991e31b08b26bd67a8cbbc400a7afc
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-13 20:34:48 +0700

    реализация 4. Перенос методов в Stream API [#279131]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,11 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        return new ArrayList<>();
+    }
+}

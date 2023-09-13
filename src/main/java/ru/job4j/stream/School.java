61fe7e77d8ed4496b70245e93d6ae66cb071bb3e
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-13 20:36:32 +0700

    test

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,11 +1,5 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-
 public class School {
-    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
-        return new ArrayList<>();
-    }
+
 }

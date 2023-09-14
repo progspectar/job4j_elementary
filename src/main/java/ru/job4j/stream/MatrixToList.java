7852db5ec70e36b56415c111768aa20bbe034bb6
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-14 19:18:21 +0700

    test

---
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -0,0 +1,11 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatrixToList {
+
+    public static List<Integer> convert(Integer[][] matrix) {
+        return new ArrayList<>();
+    }
+}

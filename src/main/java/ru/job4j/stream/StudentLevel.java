1646811c5424978a451a4461584757f449b153f6
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-15 16:41:54 +0700

    test

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -8,7 +8,7 @@ public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
                 .filter(st -> st != null)
-
+                .sorted(Comparator.comparing(Student::getScore).reversed())
                 .takeWhile(st -> st.getScore() > bound)
                 .collect(Collectors.toList());
     }
